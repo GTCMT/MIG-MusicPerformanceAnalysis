3df7dffbe746ceaa92c7026ffffb4f46153b8859
Ashis Pati <ashis.pati.89@gmail.com>
2018-11-22 14:26:49 -0500

    Added new scanScorePath() in scanning fodler. Returns the path to the midi score if it exists in the MIG-FbaData repo.

--- src/prediction_models/scanning/scanFBA.m
+++ src/prediction_models/scanning/scanFBA.m
@@ -1,13 +1,13 @@
-%% Scans the FBA dataset, returning metadata about student auditions. 
+%% Scans the FBA dataset, returning metadata about student auditions.
 % CL@GTCMT 2015
 % Metadata is returned in the form of a struct, audition_metadata, which is
 % described below.
 %
 %                         %%%% Input %%%%
 % fba_relative_path: string, the relative path from the /predictive_models
-%                    directory to the FBA2013 directory containing audio 
+%                    directory to the FBA2013 directory containing audio
 %                    files in your computer's file system.
-% band_option:       string specifying the band: 'middle', 'concert' 
+% band_option:       string specifying the band: 'middle', 'concert'
 %                    or 'symphonic'
 % instrument_option: string specifying instrument as it appears in the xls
 % segment_option: N*1 int vector, specify your target segments, ex: [3; 5]
@@ -27,31 +27,38 @@
 % file_paths, segments, and assessments have the same index for a given
 % student.
 function audition_metadata = scanFBA(fba_relative_path, band_option, ...
-                                     instrument_option, segment_option, ...
-                                     score_option, year_option)
+    instrument_option, segment_option, ...
+    score_option, year_option)
 root_path = deriveRootPath();
 full_fba_relative_path = [root_path fba_relative_path];
 
 % List the student_ids which are to be skipped
-% TODO: move this to a .txt file in the MIG-FbaData repo
+%% TODO: move this to a .txt file in the MIG-FbaData repo
 if strcmp(band_option, 'middle') == 1
     ids2skip = [29429, 32951, 42996, 43261, 44627, 56948, 39299, 39421, 41333, 42462, 43811, 44319, 61218, 29266, 33163]';
 elseif strcmp(band_option, 'symphonic') == 1
-    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935];   
+    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935];
 end
 % Figure out which students we retrive metadata for.
 % student_ids is a N X 1 vector.
 student_ids = scanStudentIds(band_option, instrument_option, year_option);
 student_ids = setdiff(student_ids, ids2skip);
 
-% Gather metadata.
+% Scan audio filepaths
 file_paths = scanFilePaths(full_fba_relative_path, student_ids, year_option);
+
+% Scan annotations for segment informations
 segments = scanSegments(segment_option, student_ids, year_option);
 segment_option_remapped = segmentRemap(segment_option, instrument_option);
+
+% Scan assessments
 assessments = scanAssessments(segment_option_remapped, student_ids, year_option);
-% TODO(Yujia)
+
+% Scan score
+% TODO - Finish the scanScore method to return the path to the midi score
 % score = scanScore(instrument_option, score_option);
-score = [];
+score_path = ...
+    scanScorePath(band_option, year_option, instrument_option, segment_option);
 pyin_paths = scanPyinPaths(student_ids, band_option, year_option);
 
 % Create the struct.
@@ -59,7 +66,7 @@ audition_metadata = struct();
 audition_metadata.file_paths = file_paths;
 audition_metadata.segments = segments;
 audition_metadata.assessments = assessments;
-audition_metadata.score = score;
+audition_metadata.score_path = score_path;
 audition_metadata.pyin_paths = pyin_paths;
 audition_metadata.student_ids = student_ids;
 end
---
+++ src/prediction_models/scanning/scanScorePath.m
@@ -0,0 +1,44 @@
+function score_path = ...
+    scanScorePath(BAND_OPTION, YEAR_OPTION, INSTRUMENT_OPTION, SEGMENT_OPTION)
+%% AP@GTCMT 2018
+% objective: returns the path to the midi score given the arguments
+% :param BAND_OPTION: string, band name, 'middle', 'concert' or 'symphonic'
+% :param YEAR_OPTION: string, year, e.g. '2013', '2014', '2015'
+% :param INSTRUMENT_OPTION: string, instrument name, 
+%                           e.g. 'Alto Saxophone', 'Bb Clarinet', 'Flute'
+% :param SEGMENT_OPTION: int, 1-5
+% :return score_path: path to the midi score, return empty matrix if file
+%                     not available
+
+slashtype = getSlashType();
+root_path = deriveRootPath();
+
+if strcmp(BAND_OPTION, 'middle')
+    band_folder = 'middleschool';
+elseif strcmp(BAND_OPTION, 'concert')
+    band_folder = 'concertband';
+elseif strcmp(YEAR_OPTION, 'symphonic')
+    band_folder = 'symphonicband';
+end
+
+if strcmp(INSTRUMENT_OPTION, 'Alto Saxophone')
+    folder_string = 'altosax';
+    file_string = [folder_string '_' num2str(SEGMENT_OPTION) '.mid' ];
+elseif strcmp(INSTRUMENT_OPTION, 'Snare')
+    folder_string = 'snare';
+    file_string = 'snare.mid';
+else
+    % TODO: complete this for other instrument types
+    score_path = [];
+    return
+end
+
+data_path = [root_path '..' slashtype '..' slashtype '..' slashtype 'MIG-FbaData'];
+score_path = [data_path slashtype 'FBA' YEAR_OPTION slashtype band_folder ...
+    slashtype 'musicalscores' slashtype folder_string slashtype file_string];
+
+if exist(score_path, 'file') ~= 2
+    score_path = [];
+end
+
+end
