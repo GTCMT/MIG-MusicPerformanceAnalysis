dca1554130e837fcd9fb7e767eb4909dee22a0bd
Ashis Pati <ashis.pati.89@gmail.com>
2019-01-14 10:11:01 -0500

    Added ids2skip for symphonic band

--- src/prediction_models/scanning/scanFBA.m
+++ src/prediction_models/scanning/scanFBA.m
@@ -34,10 +34,18 @@ full_fba_relative_path = [root_path fba_relative_path];
 
 % List the student_ids which are to be skipped
 %% TODO: move this to a .txt file in the MIG-FbaData repo
-if strcmp(band_option, 'middle') == 1
+if strcmp(band_option, 'middle')
     ids2skip = [29429, 32951, 42996, 43261, 44627, 56948, 39299, 39421, 41333, 42462, 43811, 44319, 61218, 29266, 33163]';
-elseif strcmp(band_option, 'symphonic') == 1
-    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935];
+elseif strcmp(band_option, 'symphonic')
+    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935]';
+end
+if segment_option == 4
+    if strcmp(band_option, 'middle')
+        ids2skip = [ids2skip; 34430; 53333; 53677];
+    end
+    if strcmp(band_option, 'symphonic')
+        ids2skip = [ids2skip; 45801];
+    end
 end
 % Figure out which students we retrive metadata for.
 % student_ids is a N X 1 vector.
