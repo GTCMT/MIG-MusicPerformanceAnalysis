afcf601f71219356005e363daa8e9700d2854cf5
Ashis Pati <ashis.pati.89@gmail.com>
2019-01-15 15:31:42 -0500

    Moved blacklisted student-ids to scanStudentIds method.

--- src/prediction_models/scanning/scanFBA.m
+++ src/prediction_models/scanning/scanFBA.m
@@ -32,21 +32,6 @@ function audition_metadata = scanFBA(fba_relative_path, band_option, ...
 root_path = deriveRootPath();
 full_fba_relative_path = [root_path fba_relative_path];
 
-% List the student_ids which are to be skipped
-%% TODO: move this to a .txt file in the MIG-FbaData repo
-if strcmp(band_option, 'middle')
-    ids2skip = [29429, 32951, 42996, 43261, 44627, 56948, 39299, 39421, 41333, 42462, 43811, 44319, 61218, 29266, 33163]';
-elseif strcmp(band_option, 'symphonic')
-    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935]';
-end
-if segment_option == 4
-    if strcmp(band_option, 'middle')
-        ids2skip = [ids2skip; 34430; 53333; 53677];
-    end
-    if strcmp(band_option, 'symphonic')
-        ids2skip = [ids2skip; 45801];
-    end
-end
 % Figure out which students we retrive metadata for.
 % student_ids is a N X 1 vector.
 student_ids = scanStudentIds(band_option, instrument_option, year_option);
--- src/prediction_models/scanning/scanStudentIds.m
+++ src/prediction_models/scanning/scanStudentIds.m
@@ -79,4 +79,13 @@ while isnan(num(index)) == 0
     index       = index+1;  
 end
 
+%% TODO: move this to a .txt file in the MIG-FbaData repo
+% list which studen-ids are to be skipped
+if strcmp(band_option, 'middle')
+    ids2skip = [29429, 32951, 42996, 43261, 44627, 56948, 39299, 39421, 41333, 42462, 43811, 44319, 61218, 29266, 33163, 34430, 53333, 53677]';
+elseif strcmp(band_option, 'symphonic')
+    ids2skip = [33026, 33476, 35301, 41602, 52950, 53083, 46038, 33368, 42341, 51598, 56778, 56925, 30430, 55642, 60935, 45801]';
+end
+student_ids = setdiff(student_ids, ids2skip);
+
 end
